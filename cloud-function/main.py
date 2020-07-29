d56768f1364deddc447bc7bef031f58a16b985ef
Paul Leroy <paul@idixconsulting.com>
2020-07-29 15:12:16 +0100

    Won't break gracefully

--- cloud-function/main.py
+++ cloud-function/main.py
@@ -8,11 +8,11 @@ def trigger_build(data, context):
     project_id = os.environ['PROJECT_ID']
     trigger_id = 'terraform-builder-trigger'
     source =  {"project_id": project_id,"branch_name": "master"}
-    try:
-        response = client.run_build_trigger(project_id, trigger_id, source)
-        print("Called Trigger")
-    except Exception as err:
-        traceback.print_tb(err.__traceback__)
+    #try:
+    response = client.run_build_trigger(project_id, trigger_id, source)
+    print("Called Trigger")
+    #except Exception as err:
+    #    traceback.print_tb(err.__traceback__)
     
 if __name__=="__main__":
     trigger_build(data=None,context=None)
