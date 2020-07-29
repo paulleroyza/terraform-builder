4707a8c244af5e3cdccca5c5d27aa1e73cb17776
Paul Leroy <paul@idixconsulting.com>
2020-07-29 15:32:06 +0100

    Fixed CB version at 2.0.0

--- cloud-function/main.py
+++ cloud-function/main.py
@@ -3,16 +3,16 @@ import os
 import traceback
 
 def trigger_build(data, context):
-    from google.cloud.devtools import cloudbuild_v1
-    client = cloudbuild_v1.CloudBuildClient()
+    from google.cloud.devtools import cloudbuild
+    client = cloudbuild.CloudBuildClient()
     project_id = os.environ['PROJECT_ID']
     trigger_id = 'terraform-builder-trigger'
     source =  {"project_id": project_id,"branch_name": "master"}
-    #try:
-    response = client.run_build_trigger(project_id, trigger_id, source)
-    print("Called Trigger")
-    #except Exception as err:
-    #    traceback.print_tb(err.__traceback__)
+    try:
+        response = client.run_build_trigger(project_id=project_id, trigger_id=trigger_id, source=source)
+        print("Called Trigger")
+    except Exception as err:
+        traceback.print_tb(err.__traceback__)
     
 if __name__=="__main__":
     trigger_build(data=None,context=None)
