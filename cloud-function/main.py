74e531084bb59bc1a3639ba03d19409bfb0f729a
Paul <paul@idixconsulting.com>
2020-07-29 16:18:03 +0100

    Version locked cloudbuild library and updated code to match

--- cloud-function/main.py
+++ cloud-function/main.py
@@ -9,7 +9,7 @@ def trigger_build(data, context):
     trigger_id = 'terraform-builder-trigger'
     source =  {"project_id": project_id,"branch_name": "master"}
     try:
-        response = client.run_build_trigger(project_id, trigger_id, source)
+        response = client.run_build_trigger(project_id=project_id, trigger_id=trigger_id, source=source)
         print("Called Trigger")
     except Exception as err:
         traceback.print_tb(err.__traceback__)
